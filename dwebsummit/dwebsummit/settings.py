842acc87bd6a5e4f0a7a68ce525b066a83e5b61b
Richard Caceres <richard@archive.org>
2018-07-02 11:49:17 -0700

    Update so scss files are ignored in deployment. Update sizing in people grid. Add text rendering to homepage template. Make sponsor links open in new window

--- dwebsummit/dwebsummit/settings.py
+++ dwebsummit/dwebsummit/settings.py
@@ -36,7 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'dwebsummit_frontend.apps.CustomStaticFilesConfig',
+
     'ckeditor',
     'ckeditor_uploader',
     'adminsortable2',
--- dwebsummit/dwebsummit_frontend/apps.py
+++ dwebsummit/dwebsummit_frontend/apps.py
@@ -2,7 +2,13 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
+from django.contrib.staticfiles.apps import StaticFilesConfig
 
 
 class DwebsummitFrontendConfig(AppConfig):
     name = 'dwebsummit_frontend'
+
+
+class CustomStaticFilesConfig(StaticFilesConfig):
+    """This is overridden so that scss is copied into the public directory"""
+    ignore_patterns = ['CVS', '.*', '*~', '*.scss']
